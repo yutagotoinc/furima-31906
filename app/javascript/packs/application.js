7b76be107ef17843ba7571a2e6b061647eb58602
yutagotoinc <yuta@goto-inc.jp>
2021-03-11 17:35:02 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()//この行を削除する
 require("@rails/activestorage").start()
 require("channels")
 
