ffb6343d2ac8052df637a5cbd76b115d8e72dc63
yutagotoinc <yuta@goto-inc.jp>
2021-05-17 17:23:09 +0900

    修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ApplicationRecord
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day
+  end
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
   validates :email, uniqueness: { case_sensitive: true }
-
-  VALID_BUYER_NAME_REGEX = /\A[ぁ-んァ-ヶー一-龠]+\z/
-  validates_format_of :first_name, with: VALID_BUYER_NAME_REGEX
-  validates_format_of :last_name, with: VALID_BUYER_NAME_REGEX
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     last_name_kana        {'ユウタ'}
     email                 {Faker::Internet.free_email}
     birth_day             {'1987-06-03'}
-    password = Faker::Internet.password(min_length: 6)
+    password = '1a' + Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
   end
