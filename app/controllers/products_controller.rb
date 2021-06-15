559892dd1cb967fea78e0dee6bab2b82488c78df
yutagotoinc <yuta@goto-inc.jp>
2021-06-15 10:27:05 +0900

    完了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create,:edit]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @products = Product.order("created_at DESC")
@@ -18,10 +19,34 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name,:description,:category_id,:status_id,:charge_id,:area_id,:duration_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    unless user_signed_in? && @product.user_id == current_user.id && @product.order.present?
+      redirect_to action: :index
+    end
   end
 
 end
